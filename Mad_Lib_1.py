c2044cf0632791ac1a0f7f13dcfbe18b2933ba49
AdamTurner84 <122393492+AdamTurner84@users.noreply.github.com>
2023-01-18 16:13:18 -0500

    Mad lib #1

---
+++ Mad_Lib_1.py
@@ -0,0 +1,23 @@
+# Mad Lib Generator
+
+# Questions for the User to answer
+
+noun = input('Choose a noun (Person or Thing): ')
+p_noun = input('Choose a plual noun: ')
+noun2 = input('Choose a noun (Person or Thing): ')
+place = input('Choose a place: ')
+adjective = input('Choose an adjective (Descriptive Word): ')
+noun3 = input('Choose a noun (Person or Thing): ')
+
+# Print a story from the user input
+
+print('------------------------------------------------------------------------')
+print('In the wonderful world of', place, 'lived the most amazing', p_noun, '.')
+print(noun, 'was the greatest leader of the', p_noun)
+print(noun2, 'did not like', p_noun, 'because they were too', adjective)
+print(noun, 'decided to join forces with his rival', noun3, 'to fight', noun2)
+print('The battle between', noun, 'and', noun2, 'was epic. And all the', p_noun)
+print('watched in amazement. Finally', noun3, 'jumped in and saved', noun, '.')
+print('All the', p_noun, 'cheered for', noun, 'and', noun3, '.')
+print(place, 'was once again at peace.')
+print('-------------------------------------------------------------------------')
