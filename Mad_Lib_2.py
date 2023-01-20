05f2700f6a7be0a1e56d112b133e1dcea22bcef7
AdamTurner84 <122393492+AdamTurner84@users.noreply.github.com>
2023-01-20 18:40:41 -0500

    Adding second mad lib

---
+++ Mad_Lib_2.py
@@ -0,0 +1,31 @@
+# Questions for the User to Answer
+size = input('List a unit of size: ')
+animal = input('List an animal: ')
+food = input('List a food: ')
+landscape = input('List a type of landscape: ')
+building = input('List a type of building: ')
+noun = input('List a persons name: ')
+number =input('Name a number: ')
+room = input('Name a room: ')
+verb = input('Name a verb (action word): ')
+cleaner = input('List a cleaning item: ')
+weapon = input('List a weapon: ')
+
+
+
+
+
+
+# The Story
+print('-------------------------------------------------------------')
+print('Once upon a time there lived and', size, animal,'who loved to eat', food,'.')
+print('As the', animal, 'was walking through the', landscape, 'he noticed a', building, 'in the distance.')
+print('The name of the', animal, 'was', noun,'.')
+print(noun, 'knoked on the door of the', building, 'but no one answered.')
+print(noun, 'saw a window that was open on the', number, 'floor of the', building,'.')
+print('Since everyone knows that', animal, 'are such great jumpers', noun, 'jumped to the window and crawled in.')
+print(noun, 'found that he was standing in the', room,'.')
+print('Since', noun, 'was in the', room, 'he decided to', verb,'.')
+print('As', noun, 'was', verb, 'in crashed a large', cleaner,'.')
+print(noun, 'drew out his', weapon, 'and defeated the mean', cleaner,'.')
+print('Then all of a sudden Tiffany woke up, I guess she was just dreaming.')
